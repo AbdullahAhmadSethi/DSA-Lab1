08cf5cadc7277ff3c3499a69d1655006cf1d4cd6
AbdullahAhmadSethi <kingwardon.ah@gmail.com>
2026-09-16 10:50:21 +0500

    Add files via upload

---
+++ LAB 2 Q1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // USING sizeof() TO GET THE SIZE IN BYTES 
+
+    cout << "SIZE OF INT: "    << sizeof(int)    << " bytes" << endl;
+    cout << "SIZE OF LONG: "   << sizeof(long)   << " bytes" << endl;
+    cout << "SIZE OF DOUBLE: " << sizeof(double) << " bytes" << endl;
+    cout << "SIZE OF FLOAT: "  << sizeof(float)  << " bytes" << endl;
+    cout << "SIZE OF CHAR: "   << sizeof(char)   << " bytes" << endl;
+
+    return 0;
+}
